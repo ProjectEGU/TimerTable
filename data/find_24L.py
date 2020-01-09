322e83e8e06ae268d00ea379f0eb968d6941bf0f
Chao You <youchao@dh2010pc43.utm.utoronto.ca>
2020-01-09 11:08:25 -0500

    find course by prof functionality in find_24L.py

--- data/find_24L.py
+++ data/find_24L.py
@@ -6,6 +6,22 @@ with open("course_data_utm_20199", "r") as f:
 def has_tuesday_timeslot(lec_info):
     return any([ts['weekday'] == 'TU' for ts in lec_info['timeslots']])
 
+def has_prof(prof_name, lec_secs):
+    # [[print(instructor) for instructor in sec['instructors']] for sec in lec_secs]
+    return any([prof_name.lower() in sec['instructors'].lower() for sec in lec_secs])
+
+for crs in all_crs:
+    if(crs['course_code'][8] == 'S'):
+        lec_secs = []
+        for sectype in ('LEC','PRA','TUT'):
+            if sectype in crs['course_sections']:
+                lec_secs += crs['course_sections'][sectype]
+            
+        if has_prof("ikh", lec_secs):
+            print("---------------------------------------")
+            print()
+            print("{0}".format(crs['course_code']))
+"""
 for crs in all_crs:
     if '[24L]' in crs['course_info'] and crs['course_code'][3] in ('1', '2', '3') and crs['course_code'][8] == 'S':
         lec_secs = crs['course_sections']['LEC']
@@ -17,3 +33,4 @@ for crs in all_crs:
                                                 lec_secs[0]['total_count'],
                                                 lec_secs[0]['waitlist_count']))
             print(crs['course_info'])
+"""
